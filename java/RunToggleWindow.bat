b91058c3d2885ace745f70bcef2a8ec55faf6639
Andrey <andreimipad43@mail.ru>
2024-06-21 00:50:45 +0300

    fix bat file

--- java/RunToggleWindow.bat
+++ java/RunToggleWindow.bat
@@ -1,2 +1,2 @@
 @echo off
-java -XX:+UseCompressedOops -XX:+UseG1GC -Xms1m -Xmx10m -jar C:\javaProject\ToggleWindow\out\artifacts\ToggleWindow_jar\ToggleWindow.jar
+start javaw -XX:+UseCompressedOops -XX:+UseG1GC -Xms1m -Xmx10m -jar C:\javaProject\ToggleWindow\out\artifacts\ToggleWindow_jar\ToggleWindow.jar
